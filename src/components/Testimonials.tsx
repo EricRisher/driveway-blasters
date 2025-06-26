98fb7ada9991a4dcbde92d01c049825e88d39234
EricRisher <rishereric13@gmail.com>
2025-06-25 20:13:35 -0700

    updated reviews component

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -45,34 +45,85 @@ const testimonials = [
   },
   {
     id: 6,
-    description: "They cleaned my backyard patio and we're really happy with how it turned out. They gave us the best price we could find.",
+    description:
+      "They cleaned my backyard patio and we're really happy with how it turned out. They gave us the best price we could find.",
     name: 'Will S.',
     city: 'Aliso Viejo, CA',
     source: 'Google',
-  }
+  },
 ];
 
 function Testimonials() {
   return (
-    <section className="testimonial-block">
-      <h1 className="testimonial-cta mt-4">
-        See What Your Neighbors Are Saying!
-      </h1>
-      <Carousel interval={9000}>
-        {testimonials.map((testimonial) => {
-          return (
-            <Carousel.Item key={testimonial.id}>
-              <div className="testimonial-card">
-                <p className="testimonial-description">
-                  {testimonial.description}
-                </p>
-                <h2 className="testimonial-name"> - {testimonial.name}</h2>
-                <h3 className="testimonial-city">{testimonial.city}</h3>
-              </div>
-            </Carousel.Item>
-          );
-        })}
-      </Carousel>
+    <section className="testimonial-block bg-gray-200 w-full">
+      <h3 className="mt-4 racking-widest font-light">TESTIMONIALS</h3>
+      <h2>
+        <b>Our clients love us</b>
+      </h2>
+      <h3 className="text-md mt-2 mb-6 font-light">
+        Here's some of what they had to say
+      </h3>
+      <section className="testimonial-wrapper">
+        <article className="testimonial">
+          <p>
+            I had a tenant's car leak oil onto the driveway. i contacted Jeremy
+            and he did an awesome job cleaning it. He returned my call within 5
+            minutes and was able to do the job the next day. Service and quality
+            are great. I would recommend to anyone that needs this service.
+          </p>
+          <span className="flex flex-row items-center gap-1">
+            <img src="./images/5-stars.png" alt="5 stars" className="w-32" />
+            by <b>Patrick J.</b> in Corona, CA
+          </span>
+        </article>
+
+        <article className="testimonial">
+          <p>
+            Driveway Blasters did a great job on our patio! It was covered in
+            pigeon poop after an infestation but after these guys did their
+            work, you can't even tell there ever was any. Will definitely be
+            recommending them to others.
+          </p>
+          <span className="flex flex-row items-center gap-1">
+            <img src="./images/5-stars.png" alt="5 stars" className="w-32" />
+            by <b>David M.</b> in Claremont, CA
+          </span>
+        </article>
+        <article className="testimonial">
+          <p>
+            Very satisfied customer here! I had terrible oil stains from my old
+            truck, some of them fresh - a couple of months old, and some were
+            from two years back. The older stains specifically are too hard to
+            remove. There were also a couple of gums that made my driveway look
+            really ugly. Jeremy and Eric came on time as promised and did a
+            great job. The driveway looks way better now, and the toughest
+            stains can hardly be noticed. They make up a good team, they don't
+            other reviews yet as of the time of my review, but they are
+            definitely worth trying them out!
+          </p>
+          <span className="flex flex-row items-center gap-1">
+            <img src="./images/5-stars.png" alt="5 stars" className="w-32" />
+            by <b>Ahmed Z.</b> in Irvine, CA
+          </span>
+        </article>
+      </section>
+      <div className="testimonial-button-container">
+        <button
+        className='my-4'
+          onClick={() =>
+            window.open('https://g.page/r/CYdH_rgxt-9hEBM/review', '_blank')
+          }
+        >
+          {' '}
+          <img
+            src="./icons/google-icon.webp"
+            alt="google icon"
+            width={32}
+            height={32}
+          ></img>
+          Leave us a review
+        </button>
+      </div>
     </section>
   );
 }
